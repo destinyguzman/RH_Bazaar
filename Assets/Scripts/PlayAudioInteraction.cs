71251ee952ac14e9a8715e250b1afa5654ee9536
Joanne Huang <chwong0314@me.com>
2023-01-14 14:19:18 -0800

    Reorganize files and add audio script

---
+++ Assets/Scripts/PlayAudioInteraction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioInteraction : MonoBehaviour
+{
+    public AudioSource playSound;       // Gameobject where they will be teleported to
+
+    public void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+}
